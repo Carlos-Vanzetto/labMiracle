b92bdca7ab5a074659c5325ea30e377524bdff12
Carlos Vanzetto <vanzettoc@gmail.com>
2022-08-11 02:20:16 -0300

    Adding data structures exercises modifications

--- alumnos/cv/dataStructuresExercises/linked-lists.js
+++ alumnos/cv/dataStructuresExercises/linked-lists.js
@@ -1,148 +1,145 @@
 class Node {
-  constructor(data,next=null){
-    this.data = data;
-    this.next = next;
-  }
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
 }
 class LinkedList {
-  constructor(head=null){
-    this.head = head;
-    this.size = this.getSize();
-  }
-  insert(data){
-    let node = new Node(data);
-    let current;
-    if(!this.head){
-      this.head = node;
-    }else{
-      current = this.head;
-      while(current.next){
-        current = current.next;
-      }
-      current.next = node;
-    }
-    this.size++ ; 
-  }
-  remove(value){
-    let current = this.head;
-    if(!current){
-      return;
-    }
-    let previous;
-    while(current && current.data != value){
-      previous = current;
-      current =current.next;
-    }
-    if(!current){
-      return;
-    }
-    if (!previous){
-      this.head = current.next;
-    }else{
-      previous.next=current.next;
-    }
-   this.size--;
-  }
-  clearList(){
-    this.head=null;
-    this.size=0
-  }
-  getSize() {
-    let count = 0; 
-    let node = this.head;
-    while (node) {
-        count++;
-        node = node.next
-    }
-    return count;
-  }
-  increaseSize(){
-    this.size++
-  }
-  decreaseSize(){
-    this.size--;
-  }
-  find(value){
-    let current = this.head;
-    if(!current){
-      return;
-    }
-    while(current && current.data != value){
-      current =current.next;
-    }
-    if(!current){
-      return false;
-    }
-    return current;
-  }
-  getLast(){
-    let current = this.head;
-    if(!current){
-      return;
-    }
-    while(current.next){
-      current =current.next;
-    }
-    return current;
-  }
-  // Ejercicio 2
-  invert(){
-    let current = this.head;
-    if(!current){
-      return;
-    }
-    let previous = null;
-    let next = null;
-    while(current){
-      next = current.next;
-      current.next = previous;
-      previous = current;
-      current = next;
-      }
-      this.head = previous;
-      return this.head;
-  }
-  deleteDups(){
-    let current = this.head;
-    if(!current){
-      return;
-    }
-    let arrData =[];
-    while(current){
-      arrData.push(current.data);
-      current=current.next;
-    }
-    let noDupArrData = [...new Set(arrData)]; 
-    this.clearList();
-     let i=0;
-    while(noDupArrData[i]){
-      this.insert(arrData[i]);
-      i++;
-      }
-    }
-  }
-
+	constructor(head = null) {
+		this.head = head;
+		this.size = this.getSize();
+	}
+	insert(data) {
+		let node = new Node(data);
+		let current;
+		if (!this.head) {
+			this.head = node;
+		} else {
+			current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = node;
+		}
+		this.size++;
+	}
+	remove(value) {
+		let current = this.head;
+		if (!current) {
+			return;
+		}
+		let previous;
+		while (current && current.data != value) {
+			previous = current;
+			current = current.next;
+		}
+		if (!current) {
+			return;
+		}
+		if (!previous) {
+			this.head = current.next;
+		} else {
+			previous.next = current.next;
+		}
+		this.size--;
+	}
+	clearList() {
+		this.head = null;
+		this.size = 0;
+	}
+	getSize() {
+		let count = 0;
+		let node = this.head;
+		while (node) {
+			count++;
+			node = node.next;
+		}
+		return count;
+	}
+	increaseSize() {
+		this.size++;
+	}
+	decreaseSize() {
+		this.size--;
+	}
+	find(value) {
+		let current = this.head;
+		if (!current) {
+			return;
+		}
+		while (current && current.data != value) {
+			current = current.next;
+		}
+		if (!current) {
+			return false;
+		}
+		return current;
+	}
+	getLast() {
+		let current = this.head;
+		if (!current) {
+			return;
+		}
+		while (current.next) {
+			current = current.next;
+		}
+		return current;
+	}
+	// Ejercicio 2
+	invert() {
+		let current = this.head;
+		if (!current) {
+			return;
+		}
+		let previous = null;
+		let next = null;
+		while (current) {
+			next = current.next;
+			current.next = previous;
+			previous = current;
+			current = next;
+		}
+		this.head = previous;
+		return this.head;
+	}
+	deleteDups() {
+		let current = this.head;
+		if (!current) {
+			return;
+		}
+		let arrData = [];
+		while (current) {
+			arrData.push(current.data);
+			current = current.next;
+		}
+		let noDupArrData = [...new Set(arrData)];
+		this.clearList();
+		let i = 0;
+		while (noDupArrData[i]) {
+			this.insert(arrData[i]);
+			i++;
+		}
+	}
+}
 
 // Ejercicio 3
-class DoubleLinkedList extends LinkedList{
-  constructor(head= null){
-    super(head);
-  }
-
+class DoubleLinkedList extends LinkedList {
+	constructor(head = null) {
+		super(head);
+	}
 }
-class DoubleNode extends Node{
-  constructor(data,next=null,prev=null){
-    super(data,next);
-    this.prev = prev;
-  }
+class DoubleNode extends Node {
+	constructor(data, next = null, prev = null) {
+		super(data, next);
+		this.prev = prev;
+	}
 }
-// Tests ejercicios 1 y 2  
+// Tests ejercicios 1 y 2
 let node1 = new Node(2);
 let node2 = new Node(4);
 let node3 = new Node(6);
 node1.next = node2;
 node2.next = node3;
-
 
 let testLinkedList = new LinkedList(node1);
 testLinkedList.insert(8);
@@ -152,32 +149,31 @@ testLinkedList.remove(2);
 console.log(testLinkedList.find(2));
 console.log(testLinkedList.find(4));
 console.log(`Size of testLinkedList is: ${testLinkedList.getSize()}`);
-console.log(`testLinkedList's last node is: `,testLinkedList.getLast());
+console.log(`testLinkedList's last node is: `, testLinkedList.getLast());
 let invertedList = new LinkedList(testLinkedList.invert());
 invertedList.size = invertedList.getSize();
- console.log(invertedList);
- console.log(`invertedList's last node is: `,invertedList.getLast());
+console.log(invertedList);
+console.log(`invertedList's last node is: `, invertedList.getLast());
 
- // test ejercicios 3
- 
- let dNode1 = new DoubleNode(1);
- let dNode3 = new DoubleNode(3);
- let dNode2 = new DoubleNode(2,dNode3,dNode1);
- dNode1.next=dNode2;
- dNode3.prev=dNode2;
- 
- let testDoubleLinkedList = new DoubleLinkedList(dNode1);
- console.log('Ejercicio double linked list ', testDoubleLinkedList);
- console.log("se agregan 3 nodos, un valor repetido");
- testDoubleLinkedList.insert(3);
- testDoubleLinkedList.insert(4);
- testDoubleLinkedList.insert(5);
- console.log(testDoubleLinkedList);
- console.log("se agregan 3 nodos , 3 repetidos");
- testDoubleLinkedList.insert(5);
- testDoubleLinkedList.insert(5);
- testDoubleLinkedList.insert(4);
- console.log("ejecutando deleteDup");
- testDoubleLinkedList.deleteDups();
- console.log(testDoubleLinkedList);
+// test ejercicios 3
 
+let dNode1 = new DoubleNode(1);
+let dNode3 = new DoubleNode(3);
+let dNode2 = new DoubleNode(2, dNode3, dNode1);
+dNode1.next = dNode2;
+dNode3.prev = dNode2;
+
+let testDoubleLinkedList = new DoubleLinkedList(dNode1);
+console.log("Ejercicio double linked list ", testDoubleLinkedList);
+console.log("se agregan 3 nodos, un valor repetido");
+testDoubleLinkedList.insert(3);
+testDoubleLinkedList.insert(4);
+testDoubleLinkedList.insert(5);
+console.log(testDoubleLinkedList);
+console.log("se agregan 3 nodos , 3 repetidos");
+testDoubleLinkedList.insert(5);
+testDoubleLinkedList.insert(5);
+testDoubleLinkedList.insert(4);
+console.log("ejecutando deleteDup");
+testDoubleLinkedList.deleteDups();
+console.log(testDoubleLinkedList);
--- alumnos/cv/dataStructuresExercises/queues.js
+++ alumnos/cv/dataStructuresExercises/queues.js
@@ -1,134 +1,130 @@
 class Queue {
-   
-  constructor(size){
-    this.data = new Array(size);
-    this.size = size;
-    this.rear = 0;
-    
-  }
-  enqueue(element) {
-    if(!this.isFull()) {
-          this.data[this.rear] = element;
-          this.rear = this.rear + 1;
-     }
-  }
-  dequeue() {
-    if(this.isEmpty()) {
-      return null;
-    }
-    let i = this.size;
-    while(!this.data[i]){
-      this.data[i]=null;
-      i--;
-    }
-      this.rear = this.rear-1;
-       return this.data.shift();
-    
-  }
-  length() {
-      return this.rear;
-    }
-  isFull(){
-    return this.rear === this.size;
-  }
-  isEmpty() {
-    return this.rear === 0;
-  }
+	constructor(size) {
+		this.data = new Array(size);
+		this.size = size;
+		this.rear = 0;
+	}
+	enqueue(element) {
+		if (!this.isFull()) {
+			this.data[this.rear] = element;
+			this.rear = this.rear + 1;
+		}
+	}
+	dequeue() {
+		if (this.isEmpty()) {
+			return null;
+		}
+		let i = this.size;
+		while (!this.data[i]) {
+			this.data[i] = null;
+			i--;
+		}
+		this.rear = this.rear - 1;
+		return this.data.shift();
+	}
+	length() {
+		return this.rear;
+	}
+	isFull() {
+		return this.rear === this.size;
+	}
+	isEmpty() {
+		return this.rear === 0;
+	}
 
-  getFront() {
-    if(this.isEmpty() === false) {
-        return this.data[0];
-    }
-  }
+	getFront() {
+		if (this.isEmpty() === false) {
+			return this.data[0];
+		}
+	}
 
-getLast() { 
-  if(this.isEmpty() === false) {
-       return this.data[ this.rear - 1 ] ;
-    }
-  }
-  print(){
-    if(this.isEmpty()){
-      console.log("the queue is empty");
-    }
-    let i;
-    let str = '';
-    for(i=0;i<this.rear;i++){
-      str += this.data[i] + ' ';
-    }
-    console.log(str);
-  } 
+	getLast() {
+		if (this.isEmpty() === false) {
+			return this.data[this.rear - 1];
+		}
+	}
+	print() {
+		if (this.isEmpty()) {
+			console.log("the queue is empty");
+		}
+		let i;
+		let str = "";
+		for (i = 0; i < this.rear; i++) {
+			str += this.data[i] + " ";
+		}
+		console.log(str);
+	}
 }
 
-class CircularQueue extends Queue{
-  constructor(size){
-    super(size);
-    this.front= -1;
-    this.rear = -1;
-    this.currentLenght = 0;
-  }
-  isEmpty() {
-    return this.currentLenght === 0;
-  }
-  isFull(){
-    return this.currentLenght===this.size;
-  }
-  enqueue(element) {
-    if(!this.isFull()) {
-          this.rear = (this.rear + 1) % this.size;
-          this.data[this.rear] = element;
-          this.currentLenght+=1;
-          if(this.front === -1){
-            this.front = this.rear;
-          }       
-        }
-      }
-    dequeue(){
-      if(this.isEmpty()){
-        return null;
-      }
-      const item = this.data[this.front];
-      this.data[this.front]= null;
-      this.front = (this.front + 1) % this.size;
-      this.currentLenght -= 1;
-      if(this.isEmpty()){
-        this.front = -1;
-        this.rear = -1;
-      }
-      return item;
-    }
-    print(){
-      if(this.isEmpty()){
-        console.log("Queue is empty")
-      }else{
-        let i;
-        let str = '';
-      for(i=this.front;i!==this.rear;i= (i+1) % this.size ){
-        str += this.data[i] + " ";
-
-      } 
-      str += this.data[i];
-      console.log(str);
-      }
-    }
-  }
+class CircularQueue extends Queue {
+	constructor(size) {
+		super(size);
+		this.front = -1;
+		this.rear = -1;
+		this.currentLenght = 0;
+	}
+	isEmpty() {
+		return this.currentLenght === 0;
+	}
+	isFull() {
+		return this.currentLenght === this.size;
+	}
+	enqueue(element) {
+		if (!this.isFull()) {
+			this.rear = (this.rear + 1) % this.size;
+			this.data[this.rear] = element;
+			this.currentLenght += 1;
+			if (this.front === -1) {
+				this.front = this.rear;
+			}
+		}
+	}
+	dequeue() {
+		if (this.isEmpty()) {
+			return null;
+		}
+		const item = this.data[this.front];
+		this.data[this.front] = null;
+		this.front = (this.front + 1) % this.size;
+		this.currentLenght -= 1;
+		if (this.isEmpty()) {
+			this.front = -1;
+			this.rear = -1;
+		}
+		return item;
+	}
+	print() {
+		if (this.isEmpty()) {
+			console.log("Queue is empty");
+		} else {
+			let i;
+			let str = "";
+			for (i = this.front; i !== this.rear; i = (i + 1) % this.size) {
+				str += this.data[i] + " ";
+			}
+			str += this.data[i];
+			console.log(str);
+		}
+	}
+}
 // Queue test
-  let testQueue = new Queue(5);
-  testQueue.enqueue(10);
-  testQueue.enqueue(20);
-  testQueue.enqueue(30);
-  testQueue.enqueue(40);
-  testQueue.enqueue(50);
-  testQueue.print();
-  console.log(testQueue.dequeue());
-  console.log(testQueue.dequeue());
-  testQueue.print();
-  testQueue.enqueue(60);
-  testQueue.enqueue(70);
-  console.log(testQueue.dequeue());
-  testQueue.print();
-  console.log(testQueue.dequeue());
-  console.log(testQueue.dequeue());
-  testQueue.print();
+let testQueue = new Queue(5);
+testQueue.enqueue(10);
+testQueue.enqueue(20);
+testQueue.enqueue(30);
+testQueue.enqueue(40);
+testQueue.enqueue(50);
+testQueue.print();
+console.log(testQueue.dequeue());
+console.log(testQueue.dequeue());
+testQueue.print();
+testQueue.enqueue(60);
+testQueue.enqueue(70);
+console.log(testQueue.dequeue());
+testQueue.print();
+console.log(testQueue.dequeue());
+console.log(testQueue.dequeue());
+testQueue.print();
 
 //    ***** CircularQueue test ******
 
@@ -147,26 +143,26 @@ circularQ.print();
 
 // **** Hot potatoe *****
 
-hotPotatoe = (players,randomNum)=>{
-  const circQueue = new CircularQueue(players.length);
-  const eliminatedList = [];
+hotPotatoe = (players, randomNum) => {
+	const circQueue = new CircularQueue(players.length);
+	const eliminatedList = [];
 
-  for (let i = 0; i<players.length; i++){
-    circQueue.enqueue(players[i]);
-  }
-  while(circQueue.currentLenght>1){
-    for (let i = 0; i < randomNum; i++) {
-      circQueue.enqueue(circQueue.dequeue());    
-    }
-    eliminatedList.push(circQueue.dequeue());
-  }
-  return {
-    eliminated: eliminatedList,
-    winner:circQueue.dequeue()
-  };
-}
+	for (let i = 0; i < players.length; i++) {
+		circQueue.enqueue(players[i]);
+	}
+	while (circQueue.currentLenght > 1) {
+		for (let i = 0; i < randomNum; i++) {
+			circQueue.enqueue(circQueue.dequeue());
+		}
+		eliminatedList.push(circQueue.dequeue());
+	}
+	return {
+		eliminated: eliminatedList,
+		winner: circQueue.dequeue(),
+	};
+};
 
-let currentPlayers = ["Jose","Ana","Paula","Vito","Esteban","Enrique"];
-let randomNum = Math.floor(Math.random()*20);
-const {eliminated,winner} = hotPotatoe(currentPlayers,randomNum);
-console.log(eliminated,winner);
+let currentPlayers = ["Jose", "Ana", "Paula", "Vito", "Esteban", "Enrique"];
+let randomNum = Math.floor(Math.random() * 20);
+const { eliminated, winner } = hotPotatoe(currentPlayers, randomNum);
+console.log(eliminated, winner);
